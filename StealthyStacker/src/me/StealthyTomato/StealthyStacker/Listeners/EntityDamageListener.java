0184a59fa602d77b7d4284ad7c3b6c27b91a7917
Skyew <Skyew@DESKTOP-SOV53UA>
2020-05-22 12:47:40 -0400

    Added if check to EntityDamageListener
    This ensures that the entity is in fact a mob and not a player

--- StealthyStacker/src/me/StealthyTomato/StealthyStacker/Listeners/EntityDamageListener.java
+++ StealthyStacker/src/me/StealthyTomato/StealthyStacker/Listeners/EntityDamageListener.java
@@ -13,6 +13,7 @@ import me.StealthyTomato.StealthyStacker.StackClasses.EntityStack;
 public class EntityDamageListener implements Listener {
 	@EventHandler
     public void onDamage(EntityDamageEvent event){
+		if(event.getEntity() instanceof Creature) {
         Creature mob = (Creature) event.getEntity();
         EntityStack mobStack = Main.getEntityStacks().get(mob.getUniqueId());
 	        if (mobStack != null && mobStack.getSize() > 1 && mob.getHealth() - event.getDamage() <= 0 ) {
@@ -22,5 +23,6 @@ public class EntityDamageListener implements Listener {
 	            Creature mobToDieImmediately = (Creature) mob.getWorld().spawnEntity(mob.getLocation(), mob.getType());
 	            mobToDieImmediately.damage(mob.getMaxHealth());
 	        }
-    }
+		}
+	}
 }
