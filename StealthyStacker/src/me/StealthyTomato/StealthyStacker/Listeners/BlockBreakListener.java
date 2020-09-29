1748700225e6e8ff1558bd7822db9d9136bbfb0d
Skyew <Skyew@DESKTOP-SOV53UA>
2020-09-29 12:04:19 -0400

    Revert "Only stack mob spawners"
    This reverts commit 8b8a67f1fea8a5fd14ba01c773144efeef8039f5.

--- StealthyStacker/src/me/StealthyTomato/StealthyStacker/Listeners/BlockBreakListener.java
+++ StealthyStacker/src/me/StealthyTomato/StealthyStacker/Listeners/BlockBreakListener.java
@@ -1,6 +1,5 @@
 package me.StealthyTomato.StealthyStacker.Listeners;
 
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,15 +10,13 @@ import me.StealthyTomato.StealthyStacker.StackClasses.BlockStack;
 
 public class BlockBreakListener implements Listener {
 	@EventHandler
-    public void onBlockBreak(BlockBreakEvent event) {
+    public void onBlockPlace(BlockBreakEvent event) {
 		Block block = event.getBlock();
-		if(block.getType().equals(Material.MOB_SPAWNER)) {
-			BlockStack blockStack = Main.blockStacks.get(block.getLocation());
-			if(blockStack != null) {
-				blockStack.decrementObjectStack();
-				if(blockStack.getSize() > 0)
-					event.setCancelled(true);
-			}
+		BlockStack blockStack = Main.blockStacks.get(block.getLocation());
+		if(blockStack != null) {
+			blockStack.decrementObjectStack();
+			if(blockStack.getSize() > 0)
+				event.setCancelled(true);
 		}
 	}
 }
--- StealthyStacker/src/me/StealthyTomato/StealthyStacker/Listeners/BlockPlaceListener.java
+++ StealthyStacker/src/me/StealthyTomato/StealthyStacker/Listeners/BlockPlaceListener.java
@@ -1,6 +1,5 @@
 package me.StealthyTomato.StealthyStacker.Listeners;
 
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
@@ -17,17 +16,15 @@ public class BlockPlaceListener implements Listener {
 	@EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
 		Block block = event.getBlock();
-			if(block.getType().equals(Material.MOB_SPAWNER)) {
-				BlockStack largestNearbyStack = ObjectStackUtils.findLargestBlockStackNearBlock(block);
-				if(largestNearbyStack == null) {
-					BlockStack blockStack = new BlockStack(block);
-					Main.blockStacks.put(block.getLocation(), blockStack);
-				} else if(block.getType().equals((largestNearbyStack.getPrincipalBlock()).getType())) {
-					event.setCancelled(true);
-					int newSize = largestNearbyStack.getSize() + 1;
-					largestNearbyStack.incrementObjectStack();
-					largestNearbyStack.updateName(newSize);
-				}
-			}
+		BlockStack largestNearbyStack = ObjectStackUtils.findLargestBlockStackNearBlock(block);
+		if(largestNearbyStack == null) {
+			BlockStack blockStack = new BlockStack(block);
+			Main.blockStacks.put(block.getLocation(), blockStack);
+		} else if(block.getType().equals((largestNearbyStack.getPrincipalBlock()).getType())) {
+			event.setCancelled(true);
+			int newSize = largestNearbyStack.getSize() + 1;
+			largestNearbyStack.incrementObjectStack();
+			largestNearbyStack.updateName(newSize);
 		}
 	}
+}
